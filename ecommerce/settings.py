271e0c176eba9e34c4b1ecd2789d977401e65bac
tommi11 <tbianchi2001@gmail.com>
2023-06-16 20:21:32 +0200

    Deploying To Railway

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,7 +12,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path, dj_database_url
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,7 +81,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        dj_database_url.config(default='sqlite:///db.sqlite3')
     }
 }
 
